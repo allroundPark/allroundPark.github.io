2f8605b034ec5fa3692a79b4cd331d1afdb95f47
Juhyeong Park <wngud913@gmail.com>
2021-08-19 17:11:00 +0900

    Add files via upload

---
+++ components/MainPage.js
@@ -0,0 +1,20 @@
+import Navbar from './Navbar.js';
+import About from './About.js';
+import News, {NewsItems, handleNewsFilter} from './News.js';
+import Projects, {ProjectItems, handleProjectFilter} from './Projects.js';
+
+export default function MainPage(data){
+    
+    document.querySelector('.container').innerHTML = `
+        
+        ${About(data.about)}
+        
+        ${Projects(data.projects)}
+
+        ${News(data.news)}
+    `
+    
+    handleNewsFilter(data);
+    handleProjectFilter(data);
+}
+
